9a47ded1783a3c7b1583e9e2e4ba957dbf7ac207
NatashaSetiadi <natset456@gmail.com>
2022-09-04 01:41:54 -0400

    Got target and target range to update using textbox

--- NPRL_GUI.py
+++ NPRL_GUI.py
@@ -27,11 +27,20 @@ frame_right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
 label_left = customtkinter.CTkLabel(master=frame_left,
                                     text="NPRL Force Sensor GUI",
                                     text_font=("Roboto Medium", -16))
+target = 0.1
+target_range = 0.1
+def button_event():
+    global target, target_range
+    print("click")
+    target = float(entry_target.get())
+    target_range = float(entry_range.get())
 label_left.grid(row=1, column=0, pady=10, padx=10)
 entry_target = customtkinter.CTkEntry(master=frame_left, placeholder_text="Target")
-entry_target.grid(row=5, column=0, pady=10, padx=10)
+entry_target.grid(row=4, column=0, pady=10, padx=10)
 entry_range = customtkinter.CTkEntry(master=frame_left, placeholder_text="Range from target")
-entry_range.grid(row=6, column=0, pady=10, padx=10)
+entry_range.grid(row=5, column=0, pady=10, padx=10)
+button = customtkinter.CTkButton(master=frame_left, text="Submit", command=button_event)
+button.grid(row=6, column=0, pady=10, padx=10)
 
 frame_right.rowconfigure((0, 1, 2, 3), weight=1)
 frame_right.rowconfigure(7, weight=20)
@@ -47,18 +56,16 @@ progressbar.grid(row=2, column=0, sticky="ew", padx=15, pady=15)
 progressbar.configure(progress_color='#e3141f')
 
 label_info_1 = customtkinter.CTkLabel(master=frame_info,
-                                                   text="NPRL Force Sensor Output",
-                                                   height=100,
-                                                   corner_radius=3,  # <- custom corner radius
-                                                   justify=tkinter.LEFT,
-                                                   text_font=("Roboto Medium", -16))
+                                      text="NPRL Force Sensor Output",
+                                      height=100,
+                                      corner_radius=3,
+                                      justify=tkinter.LEFT,
+                                      text_font=("Roboto Medium", -16))
 label_info_1.grid(column=0, row=0, sticky="we", padx=5, pady=5)
 displayLab = Label(frame_info, textvariable=displayVar, background='#333333', fg="white")
 displayLab.grid(column=0, row=1, sticky="we", padx=5, pady=5)
 progressbar.set(0)
 
-def change_appearance_mode(self, new_appearance_mode):
-    customtkinter.set_appearance_mode(new_appearance_mode)
 def arduino_handler():
     global current_progress
     while True:
@@ -67,7 +74,11 @@ def arduino_handler():
         current_progress = float(data)
         if 0 <= current_progress < 1:
             progressbar.set(current_progress)
-        if current_progress > 0.1:
+        target_subtact = target - target_range
+        target_add = target + target_range
+        #print(target_subtact)
+        #print(target_add)
+        if target_subtact < current_progress < target_add:
             progressbar.configure(progress_color='#2bf09e')
         else:
             progressbar.configure(progress_color='#e3141f')
